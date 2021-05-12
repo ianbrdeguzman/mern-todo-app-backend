09260f3c2cf2099d7690ee7e9660e002c4a3ce18
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-05-11 21:16:24 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Todo = require('./models/Todo');
+
+mongoose.connect(
+    'mongodb+srv://admin:admin123@cluster0.0yc2m.mongodb.net/todosDB',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+mongoose.connection.once('open', () => {
+    console.log('Mongodb connection successful');
+});
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    Todo.find().then((todos) => res.json(todos));
+});
+
+app.post('/create', (req, res) => {
+    const todo = new Todo(req.body);
+    todo.save()
+        .then((todo) => {
+            res.json(todo);
+        })
+        .catch((err) => {
+            res.status(500).send(err.message);
+        });
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    Todo.findById(id, (err, todo) => {
+        res.json(todo);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
